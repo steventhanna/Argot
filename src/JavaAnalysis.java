1f5333b97b1e52e438af26711c152a5c2ebfe964
steventhanna <steventhanna@gmail.com>
2015-06-02 23:37:17 -0500

    Added multiple author support

--- src/JavaAnalysis.java
+++ src/JavaAnalysis.java
@@ -35,6 +35,16 @@ public class JavaAnalysis {
   private String[] description;
 
   /**
+  * @description Author of class
+  */
+  private String[] author;
+
+  /**
+  * @description ArrayList<String> of the extracted header
+  */
+  private ArrayList<String> header = new ArrayList<String>();
+
+  /**
   * @description Constructor for JSAnalysis
   * @param DPFIle file - Predetermined JS File
   */
@@ -58,7 +68,7 @@ public class JavaAnalysis {
       System.out.println("Header does not exist");
       return;
     } else {
-      ArrayList<String> header = new ArrayList<String>();
+      header = new ArrayList<String>();
       // Strip all blank space and transfer header
       for(int i = 0; i < classDeclaration; i++) {
         if(contents.get(i) != "") {
@@ -76,12 +86,46 @@ public class JavaAnalysis {
     }
   }
 
+  public void extractAuthor() {
+    ArrayList<String> authors = new ArrayList<String>();
+    for(int i = 0; i < header.size(); i++) {
+      // System.out.println(header.get(i).contains("@author"));
+      if(header.get(i).contains("@author")) {
+        authors.add(header.get(i));
+        // System.out.println(header.get(i));
+      }
+    }
+    author = new String[authors.size()];
+    for(int i = 0; i < authors.size(); i++) {
+      // Remove @author
+      author[i] = removeTag("@author", authors.get(i));
+      System.out.println(author[i]);
+    }
+  }
 
+  /**
+  * Remove the tag from a specified string.
+  * The tag must be included within the string
+  * @param String tag - the tag to be removed
+  * @param String s - the String that the tag must be removed from
+  * @return String - the string sans tag
+  */
+  public String removeTag(String tag, String s) {
+    // There should be white space after the tag... If not the user fucked up
+    // Take into account the * space
+    int tagLength = tag.length() + 3;
+    return s.substring(tagLength);
+  }
+
+  public void go() {
+    extractHeader();
+    extractAuthor();
+  }
 
 
   public static void main(String[] args) {
     ArgotFile file = new ArgotFile("/Users/steventhanna/Desktop/BiggestDir.java");
     JavaAnalysis analyze = new JavaAnalysis(file);
-    analyze.extractHeader();
+    analyze.go();
   }
 }
