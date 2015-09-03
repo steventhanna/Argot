e60eddf4989bf25b45d17c5eef45d3cb83650a6c
steventhanna <steventhanna@gmail.com>
2015-09-03 16:41:25 -0400

    Failed implementation of a extractMethod

--- src/JavaFile.java
+++ src/JavaFile.java
@@ -92,6 +92,35 @@ public class JavaFile extends ArgotFile {
   /** @end */
 
   /**
+  * Find the nearest matching bracket
+  * @param int lineNumber - the line number of the opening bracket
+  * @return
+  */
+  public int nextBracket(int openBracketLineNumber, int beginCounter, int endCounter) {
+    int result = 0;
+    // Find next line number
+    for(int i = openBracketLineNumber; i < body.size(); i++) {
+      // Check the last char for a bracket.  Comments not excluded
+      String content = body.get(i);
+      if(content.charAt(content.length()) == '{') {
+        beginCounter++;
+        return nextBracket(i, beginCounter, endCounter);
+      } else if(content.charAt(content.length()) == '}') {
+        endCounter++;
+        result = i;
+        if(endCounter == beginCounter) {
+          return result;
+        } else {
+          System.out.println("There was an error identifying the next bracket.");
+        }
+      } else {
+        return nextBracket(openBracketLineNumber++, beginCounter, endCounter);
+      }
+    }
+    return 0;
+  }
+
+  /**
   * Recognize a method in a given block of code.
   * Methods will always have a opening an closing parenthese.
   * Additioanlly, methods should also have a public or a private.
@@ -110,11 +139,20 @@ public class JavaFile extends ArgotFile {
             startingPosition.add(i);
           }
         }
-        if(content.contains("@end")) {
-          endingPosition.add(i);
-        }
+        // if(content.contains("@end")) {
+          // endingPosition.add(i);
+        // }
       }
     }
+
+    // Here is where it gets interesting
+    // We need to match the brackets {}
+
+    for(int i = startingPosition.get(0); i > startingPosition.size(); i++) {
+      nextBracket(i);
+    }
+
+
     // Check that startin position and ending position ararylists are same size
     if(startingPosition.size() == endingPosition.size()) {
       System.out.println("Starting position and Ending Position size: " + startingPosition.size());
