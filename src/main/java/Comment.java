b8f4b1d6c5fd42f9dd6a0d47a4f2be3a1e2ca87d
steventhanna <steventhanna@gmail.com>
2016-08-20 10:23:56 -0500

    README updated, lots of redesign done, need to finish comment.clean

--- src/main/java/Comment.java
+++ src/main/java/Comment.java
@@ -10,7 +10,143 @@
 import java.util.ArrayList;
 
 
-public interface Comment {
+public class Comment {
+
+  /**
+  * @type :: VAR
+  * @name :: type
+  * @description :: The type of the specific comment
+  */
+  private String type;
+
+  /**
+  * @type :: VAR
+  * @name :: rawData
+  * @description :: the raw strings directly from the constructor
+  */
+  private ArrayList<String> rawData = new ArrayList<String>();
+
+  /**
+  * @type :: VAR
+  * @name :: cleanedComments
+  * @description :: the sanitized version of the comments
+  */
+  private ArrayList<String> cleanedComments = new ArrayList<String>();
+
+  public Comment(ArrayList<String> rawData, String commentStyle) {
+    this.rawData = rawData;
+    clean(commentStyle);
+    extractType();
+  }
+
+  public Comment(ArrayList<String> rawData, String commentStyle, String beginStyle, String endStyle) {
+    this.rawData = rawData;
+    clean(commentStyle, beginStyle, endStyle);
+    extractType();
+  }
+
+  public Comment(ArrayList<String> rawData, String commentStyle, String beginStyle, String endStyle, String commentEndStyle) {
+    this.rawData = rawData;
+    clean(commentStyle, beginStyle, endStyle, commentEndStyle);
+    extractType();
+  }
+
+  /**
+  * @type :: FUNC
+  * @name :: extractType
+  * @description :: Extracts the type of the function.
+  */
+  public void extractType() {
+    String gettingTag = "";
+    for(int i = 0; i < cleanedComments.size(); i++) {
+      if(cleanedComments.get(i).toLowerCase().contains("@type")) {
+        gettingTag = cleanedComments.get(i).toLowerCase();
+        break;
+      }
+    }
+    if(!gettingTag.equals("")) {
+      String[] tagArr = gettingTag.split("::");
+      if(tagArr.length == 2) {
+        type = Utility.removeWhitespace(tagArr[1].toLowerCase());
+      }
+    }
+  }
+
+  /**
+  * @type :: FUNC
+  * @name :: strip
+  * @description :: Strips whitespace, and other language specific comments
+  * from the given String
+  * @param :: String - the string to strip the LSC's from
+  * @param :: String commentStyle - the commenting style to remove
+  * @param :: String beginStyle - the specific beginning style of the comment to remove
+  * @param :: String endStyle - the specific ending style of the comment to remove
+  * @param :: String commentEndStyle - the specific comment ending to remove like in HTML
+  * @return :: String - The cleaned string
+  */
+  public String strip(String s, String commentStyle, String beginStyle, String endStyle, String commentEndStyle) {
+    // Strip the whitespace from the beginning
+    s = s.trim();
+
+    // Specific conditions
+    if(beginStyle == null || endStyle == null) {
+      if(s.length() >= commentStyle.length() && s.substring(0, commentStyle.length()).equals(commentStyle)) {
+        s = s.substring(commentStyle.length());
+      }
+      s = s.trim();
+    } else {
+      if(beginStyle != null && endStyle != null) {
+        if(s.length() >= beginStyle.length() && s.substring(0, beginStyle.length()).equals(beginStyle)) {
+          s = " ";
+        } else if(s.equals(endStyle)) {
+          s = " ";
+        } else if(s.length() >= commentStyle.length() && s.substring(0, commentStyle.length()).equals(commentStyle)) {
+          s = s.substring(commentStyle.length());
+        }
+      }
+      if(commentEndStyle != null) {
+        if(s.length() > commentEndStyle.length() && s.substring(s.length() - commentEndStyle.length()).equals(commentEndStyle)) {
+          s = s.substring(0, s.length() - commentEndStyle.length());
+        }
+      }
+    }
+    s = s.trim();
+    return s;
+  }
+
+  /**
+  * @type :: FUNC
+  * @name :: clean
+  * @description :: removes commenting styles from the arraylist
+  * @param :: String commentStyle - the comment style to remove from the string
+  */
+  public void clean(String commentStyle) {
+  }
+
+  /**
+  * @type :: FUNC
+  * @name :: clean
+  * @description :: removes commenting styles from the arraylist
+  * @param :: String commentStyle - the comment style to remove from the string
+  * @param :: String beginStyle - the style that starts the comment set
+  * @param :: String endStyle - the style that ends the comment set
+  */
+  public void clean(String commentStyle, String beginStyle, String endStyle) {
+
+  }
+
+  /**
+  * @type :: FUNC
+  * @name :: clean
+  * @description :: removes commenting styles from the arraylist
+  * @param :: String commentStyle - the comment style to remove from the string
+  * @param :: String beginStyle - the style that starts the comment set
+  * @param :: String endStyle - the style that ends the comment set
+  * @param :: String commentEndStyle - the specific ending of a comment like in HTML
+  */
+  public void clean(String commentStyle, String beginStyle, String endStyle, String commentEndStyle) {
+
+  }
 
   /**
   * @type :: FUNC
@@ -19,7 +155,9 @@ public interface Comment {
   * comments from the constructor
   * @note :: Big ass switch statement here
   */
-  public void extract();
+  public void extract() {
+
+  }
 
   /**
   * @type :: FUNC
@@ -29,6 +167,8 @@ public interface Comment {
   * @note :: Might have to switch to a different data-structure as
   * a String might not be large enough
   */
-  public String generateMarkdown();
+  public String generateMarkdown() {
+    return null;
+  }
 
 }
--- src/main/java/Language.java
+++ src/main/java/Language.java
@@ -5,17 +5,19 @@
 * @class :: Language
 * @description :: An abstract class to make sure that future commenting languages
 * all follow the same format.
+* @end
 */
 
 import java.util.ArrayList;
 
 
-public abstract class Language {
+public class Language {
 
   /**
   * @type :: VAR
   * @name :: rawComments
   * @description :: String Representaion of the comments
+  * @end
   */
   public ArrayList<String> rawComments = new ArrayList<String>();
 
@@ -23,6 +25,7 @@ public abstract class Language {
   * @type :: VAR
   * @name :: comments
   * @description :: Object representation of comments
+  * @end
   */
   public ArrayList<SlashComment> comments = new ArrayList<SlashComment>();
 
@@ -31,6 +34,7 @@ public abstract class Language {
   * @name :: functions
   * @description :: Oject representation of all the function comments. Used
   * as staging before Markdown creation.
+  * @end
   */
   public ArrayList<FunctionComment> functions = new ArrayList<FunctionComment>();
 
@@ -39,6 +43,7 @@ public abstract class Language {
   * @name :: variables
   * @description :: Object representation of all the variable comments.  Used
   * as a staging before Markdown creation.
+  * @end
   */
   public ArrayList<VariableComment> variables = new ArrayList<VariableComment>();
 
@@ -47,6 +52,7 @@ public abstract class Language {
   * @name :: classes
   * @description :: Object representation of all the class comments.  Used
   * as a staging before Markdown creation.
+  * @end
   */
   public ArrayList<ClassComment> classes = new ArrayList<ClassComment>();
 
@@ -54,14 +60,68 @@ public abstract class Language {
   * @type :: VAR
   * @name :: renderedMarkdown
   * @description :: The rendered markdown in the order that the comments appear in the target file.
+  * @end
   */
   private ArrayList<String> renderedMarkdown = new ArrayList<String>();
+
+  /**
+  * @type :: FUNC
+  * @name :: Language
+  * @description :: The most general language constructor, takes the raw contents of the file and the commenting style
+  * @param :: ArrayList<String> contents - the raw contents of the file to be parsed
+  * @param :: String commentStyle - the comment style that is used to identify comments in the specific
+  * file
+  * @end
+  */
+  public Language(ArrayList<String> contents, String commentStyle) {
+    extractComments(contents, commentStyle);
+    delegateComments();
+    renderMarkdown();
+  }
+
+  /**
+  * @type :: FUNC
+  * @name :: Language
+  * @description :: A specialized language constructor. Specifies a begin and end comment style, like
+  * traditional JavaDocs.
+  * @param :: ArrayList<String> contents - the raw contents of the file to be parsed
+  * @param :: String commentStyle - the comment style that is used to identify comments in the specific
+  * file
+  * @param :: String beginStyle - the beginning style of a comment. For Java it would be `/**`
+  * @param :: String endStyle - the ending style of a comment. No demo bc it would mess up the comment
+  * @end
+  */
+  public Language(ArrayList<String> contents, String commentStyle, String beginStyle, String endStyle) {
+    extractComments(contents, commentStyle, beginStyle, endStyle);
+    delegateComments();
+    renderMarkdown();
+  }
+
+  /**
+  * @type :: FUNC
+  * @name :: Language
+  * @description :: A specialized language constructor. Specifies a begin and end comment style, like
+  * traditional JavaDocs, in addition to end of a comment for HTML like files.
+  * @param :: ArrayList<String> contents - the raw contents of the file to be parsed
+  * @param :: String commentStyle - the comment style that is used to identify comments in the specific
+  * file
+  * @param :: String beginStyle - the beginning style of a comment. For Java it would be `/**`
+  * @param :: String endStyle - the ending style of a comment. No demo bc it would mess up the comment
+  * @param ;: String commentEndStyle - the end of a comment style `<!-- test -->`. In that case it would be `-->`
+  * @end
+  */
+  public Language(ArrayList<String> contents, String commentStyle, String beginStyle, String endStyle, String commentEndStyle) {
+    extractComments(contents, commentStyle, beginStyle, endStyle, commentEndStyle);
+    delegateComments();
+    renderMarkdown();
+  }
 
   /**
   * @type :: FUNC
   * @name :: delegateComments
   * @description :: Based on the type of the comment, build the associated
   * object.
+  * @end
   */
   public void delegateComments() {
     for(int i = 0; i < comments.size(); i++) {
@@ -102,14 +162,125 @@ public abstract class Language {
   * For now, the extracted comments both get added to individual comment
   * objects as well as the raw ArrayList
   * @param :: ArrayList<String> - the contents of the file
+  * @param :: String commentStyle - the identifier for the parser to take out
+  * @end
   */
-  public abstract void extractComments(ArrayList<String> contents);
+  public void extractComments(ArrayList<String> contents, String commentStyle) {
+    for(int i = 0; i < contents.size(); i++) {
+      ArrayList<String> commentBuffer = new ArrayList<String>();
+      // Check if a valid comment
+      if (contents.get(i).contains(commentStyle) && contents.get(i).contains("@type")) {
+        // Continue adding comments until a end tag is found
+        rawComments.add(contents.get(i));
+        commentBuffer.add(contents.get(i));
+        for(int j = i; j < contents.size(); j++) {
+          if (contents.get(j).contains(commentStyle) && contents.get(j).contains("@end") == false) {
+            rawComments.add(contents.get(j));
+            commentBuffer.add(contents.get(j));
+          } else if (contents.get(j).contains(commentStyle) && contents.get(j).contains("@end")) {
+            rawComments.add(contents.get(j));
+            commentBuffer.add(contents.get(j));
+            comments.add(new Comment(commentBuffer, commentStyle));
+            i = j;
+            break;
+          } else {
+            i = j;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  /**
+  * @type :: FUNC
+  * @name :: extractComments
+  * @description :: Extract the comment strings from the raw content.
+  * For now, the extracted comments both get added to individual comment
+  * objects as well as the raw ArrayList
+  * @param :: ArrayList<String> - the contents of the file
+  * @param :: String commentStyle - the identifier for a comment
+  * @param :: String beginStyle - the identifier for a beginning comment set if the comments beginning, middle,
+  * and end are different.
+  * @param :: String endStyle - the identifier for a ending comment set if the comment beginning, middle, and end
+  * are different.
+  * @end
+  */
+  public void extractComments(ArrayList<String> contents, String commentStyle, String beginStyle, String endStyle) {
+    for(int i = 0; i < contents.size(); i++) {
+      ArrayList<String> commentBuffer = new ArrayList<String>();
+      // Check if a valid comment
+      if (contents.get(i).contains(beginStyle)) {
+        // Continue adding comments until a end tag is found
+        rawComments.add(contents.get(i));
+        commentBuffer.add(contents.get(i));
+        for(int j = i; j < contents.size(); j++) {
+          if (contents.get(j).contains(commentStyle)) {
+            rawComments.add(contents.get(j));
+            commentBuffer.add(contents.get(j));
+          } else if (contents.get(j).contains(endStyle)) {
+            rawComments.add(contents.get(j));
+            commentBuffer.add(contents.get(j));
+            comments.add(new Comment(commentBuffer, commentStyle, beginStyle, endStyle));
+            i = j;
+            break;
+          } else {
+            i = j;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  /**
+  * @type :: FUNC
+  * @name :: extractComments
+  * @description :: Extract the comment strings from the raw content.
+  * For now, the extracted comments both get added to individual comment
+  * objects as well as the raw ArrayList
+  * @param :: ArrayList<String> - the contents of the file
+  * @param :: String commentStyle - the identifier for a comment
+  * @param :: String beginStyle - the identifier for a beginning comment set if the comments beginning, middle,
+  * and end are different.
+  * @param :: String endStyle - the identifier for a ending comment set if the comment beginning, middle, and end
+  * are different.
+  * @param :: String commentEndStyle - the identifier for a comment ending such as the `-->` in HTML
+  * @end
+  */
+  public void extractComments(ArrayList<String> contents, String commentStyle, String beginStyle, String endStyle, String commentEndStyle) {
+    for(int i = 0; i < contents.size(); i++) {
+      ArrayList<String> commentBuffer = new ArrayList<String>();
+      // Check if a valid comment
+      if (contents.get(i).contains(beginStyle)) {
+        // Continue adding comments until a end tag is found
+        rawComments.add(contents.get(i));
+        commentBuffer.add(contents.get(i));
+        for(int j = i; j < contents.size(); j++) {
+          if (contents.get(j).contains(commentStyle)) {
+            rawComments.add(contents.get(j));
+            commentBuffer.add(contents.get(j));
+          } else if (contents.get(j).contains(endStyle)) {
+            rawComments.add(contents.get(j));
+            commentBuffer.add(contents.get(j));
+            comments.add(new Comment(commentBuffer, commentStyle, beginStyle, endStyle, commentEndStyle));
+            i = j;
+            break;
+          } else {
+            i = j;
+            break;
+          }
+        }
+      }
+    }
+  }
 
   /**
   * @type :: FUNC
   * @name :: getRawComments
   * @description :: Getter for the rawComment ArrayList
   * @return :: ArrayList<String> - the comments to return
+  * @end
   */
   public ArrayList<String> getRawComments() {
     return rawComments;
@@ -119,6 +290,7 @@ public abstract class Language {
   * @type :: FUNC
   * @name :: renderMarkdown
   * @description :: Renders the markdown for the language
+  * @end
   */
   public void renderMarkdown() {
     if(classes.size() > 0) {
@@ -147,6 +319,7 @@ public abstract class Language {
   * @name :: getRenderedMarkdown
   * @description :: Returns the rendered markdown
   * @return ;: ArrayList<String> - the rendered markdown of the file
+  * @end
   */
   public ArrayList<String> getRenderedMarkdown() {
     return renderedMarkdown;
